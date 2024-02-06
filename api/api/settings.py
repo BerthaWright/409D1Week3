b9fef71c53c242dcade68018ab165303bc89f6ae
Bertha Wright <bmwrigh2@coastal.edu>
2024-02-06 09:57:30 -0500

    Configured postgres adapter

--- api/api/settings.py
+++ api/api/settings.py
@@ -9,6 +9,15 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST' : os.environ.get('DB_HOST'),
+        'NAME' : os.environ.get('DB_NAME'),
+        'USER' : os.environ.get('DB_USER'),
+        'PASSWORD' : os.environ.get('DB_PASS'),
+    }
+}
 
 from pathlib import Path
 
