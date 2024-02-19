1eb756f3b0102b49e97860db34ad00fb34b74e78
BerthaWright <74274285+BerthaWright@users.noreply.github.com>
2024-02-19 13:54:34 -0500

    Postman Steps

---
+++ api/core/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from core.views import AirportViewSet, AirlineViewSet, RunwayViewSet, FlightViewSet
+
+router = DefaultRouter()
+router.register(r'airports', AirportViewSet)
+router.register(r'airlines', AirlineViewSet)
+router.register(r'runways', RunwayViewSet)
+router.register(r'flights', FlightViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
