e612752fd5ff4127e8b5da2fab75f1c402adcb3b
BerthaWright <74274285+BerthaWright@users.noreply.github.com>
2024-04-16 16:41:27 -0400

    Week 13 Setup

--- airport-info/src/app/page.tsx
+++ airport-info/src/app/page.tsx
@@ -5,10 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+      <p>Hello from Home!</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -31,7 +28,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="/next.svg" 
           alt="Next.js Logo"
           width={180}
           height={37}
